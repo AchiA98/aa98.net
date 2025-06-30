0b6cd7d53551769ef4c1a4f4d1828ae9206138f5
Adrian Altner <altner@mailbox.org>
2025-06-30 07:12:57 +0200

    fix: layout

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,16 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
+import './styles/global.css'
+import typescriptLogo from '/typescript.svg'
 import viteLogo from '/vite.svg'
 
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div class="container logos">
     <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="${viteLogo}" class="logo vite" alt="Vite logo" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
-
   </div>
 `
 
